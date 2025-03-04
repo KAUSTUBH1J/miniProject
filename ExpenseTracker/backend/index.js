ad2198a29b0865b1ec6fe9c7a5c727ee4c2dd6f5
kaustubh.jadhav <kaustubh.jadhav@esds.co.in>
2025-03-04 09:56:49 +0530

    add validation

--- ExpenseTracker/backend/index.js
+++ ExpenseTracker/backend/index.js
@@ -3,32 +3,44 @@ const ExpenseModel = require('./config/dbConnection');
 
 const app = express();
 
-app.get('/getData',async (req,resp)=>{
+const validateData = (req, res, next) => {
+    const { title, amount, category_id } = req.body;
+    if (!title || typeof title !== 'string') {
+      return res.status(400).json({ error: 'Title is required and must be a string' });
+    }
+    if (!amount || typeof amount !== 'number') {
+      return res.status(400).json({ error: 'Amount is required and must be a number' });
+    }
+    if (!category_id || typeof category_id !== 'number') {
+        return res.status(400).json({ error: 'category_id is required and must be a number' });
+    }
+    next();
+};
+
+
+app.get('/getData',  async (req,resp)=>{
     let data = await ExpenseModel.find({});
     resp.send(data);
 });
 
-app.get('/insert', async (req, resp)=>{
-    let data = new ExpenseModel({
-        // title: 12,
-        amount: 45000,
-        category_id: 1
-    });
+app.get('/insert',validateData, async (req, resp)=>{
+    console.log(req.body);
+    
+    // let data = new ExpenseModel({
+    //     title: 'hello',
+    //     amount: 45000,
+    //     category_id: 1
+    // });
     try{
-        let result = await data.save();
-        resp.send(result);
+        // let result = await data.save();
+        // resp.send(result);
     }catch(error){
+        console.log('Error -->')
         console.log(error)   
     }
 })
 
 
-const insert = async () =>{
-    
-    
-}
-
-// insert();
 
 
 app.listen(4000)
